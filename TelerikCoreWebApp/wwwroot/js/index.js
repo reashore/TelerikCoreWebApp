89ad4785af6af88cfd9d6e013d9cb9e76b9307ab
CLBCLAB\frank.reashore <frank.reashore@gmail.com>
2017-10-06 14:25:20 -0700

    Started window management

--- TelerikCoreWebApp/wwwroot/js/index.js
+++ TelerikCoreWebApp/wwwroot/js/index.js
@@ -1,4 +1,151 @@
-﻿
+﻿"use strict";
+
+//-----------------------------------------------------
+
+//var numberItems = 6;
+
+//var popupWindowIdArray = ["popupWindow1", "popupWindow2", "popupWindow3", "popupWindow4", "popupWindow5", "popupWindow6"];
+
+//var popupWindows = popupWindowIdArray.forEach(function(item) {
+//    $(item);
+//});
+
+var popupWindowArray = [];
+
+var createPopupWindowArray = function (numberItems) {
+    var popupWindowArray = [];
+
+    for (var n = 1; n <= numberItems; n++) {
+        popupWindowArray.push($("#popupWindow" + n));
+    }
+
+    return popupWindowArray;
+}
+
+//-----------------------------------------------------
+
+var defaultPopupWindowConfiguration = {
+    position: {
+        top: 200,
+        left: 600
+    },
+    width: 600,
+    height: 300,
+    maxHeight: 700,
+    maxWidth: 700,
+    draggable: true,
+    iframe: true,
+    modal: false,
+    scrollable: true,
+    visible: false,
+    actions: ["Pin", "Refresh", "Minimize", "Maximize", "Close"],
+    open: onOpen,
+    activate: onActivate,
+    refresh: onRefresh,
+    resize: onResize,
+    dragstart: onDragStart,
+    dragend: onDragEnd,
+    deactivate: onDeactivate,
+    minimize: onMinimize,
+    maximize: onMaximize,
+    close: onClosePopupWindow
+};
+
+//-----------------------------------------------------
+
+var popupWindow1Configuration = {
+    title: "Popup Window1",
+    content: "/Home/Index1"
+};
+
+var popupWindow2Configuration = {
+    title: "Popup Window2",
+    content: "/Home/Index2"
+};
+
+var popupWindow3Configuration = {
+    title: "Popup Window3",
+    content: "/Home/Index3"
+};
+
+var popupWindow4Configuration = {
+    title: "Popup Window4",
+    content: "/Home/Index4"
+};
+
+var popupWindow5Configuration = {
+    title: "Popup Window5",
+    content: "/Home/Index5"
+};
+
+var popupWindow6Configuration = {
+    title: "Popup Window6",
+    content: "/Home/Index6"
+};
+
+//-----------------------------------------------------
+
+var getPopupWindowConfiguration = function (menuItemText) {
+    var popupWindowconfiguration = null;
+
+    switch(menuItemText) {
+        case "Menu1 Item1":
+            popupWindowconfiguration = popupWindow1Configuration;
+            break;
+
+        case "Menu1 Item2":
+            popupWindowconfiguration = popupWindow2Configuration;
+            break;
+
+        case "Menu1 Item3":
+            popupWindowconfiguration = popupWindow3Configuration;
+            break;
+
+        case "Menu2 Item1":
+            popupWindowconfiguration = popupWindow4Configuration;
+            break;
+
+        case "Menu2 Item2":
+            popupWindowconfiguration = popupWindow5Configuration;
+            break;
+
+        case "Menu2 Item3":
+            popupWindowconfiguration = popupWindow6Configuration;
+            break;
+
+        //case "Menu3 Item1":
+        //    popupWindowconfiguration = $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration);
+        //    break;
+
+        //case "Menu3 Item2":
+        //    popupWindowconfiguration = $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration);
+        //    break;
+
+        //case "Menu3 Item3":
+        //    popupWindowconfiguration = $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration);
+        //    break;
+    }
+
+    $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration);
+
+    return popupWindowconfiguration;
+};
+
+//var popupWindowsConfiguration = {
+//    "Menu1 Item1": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+//    "Menu1 Item2": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+//    "Menu1 Item3": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+
+//    "Menu2 Item1": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+//    "Menu2 Item2": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+//    "Menu2 Item3": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+
+//    "Menu3 Item1": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+//    "Menu3 Item2": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration),
+//    "Menu3 Item3": $.extend(popupWindow1Configuration, defaultPopupWindowConfiguration)
+//};
+
+//-------------------------------------------------------------
 // Functions
 
 var extractNumericSuffix = function (value, prefix) {
@@ -29,7 +176,19 @@ var onClickMenuItem = function (e) {
     menuItem.prop("disabled", true);
 
     var $popupWindow = $("#popupWindow" + data);
-    var popupWindow = $popupWindow.data("kendoWindow");
+    $popupWindow.empty();
+    var windowConfiguration = {
+        title: "Popup Window New",
+        resizable: true,
+        width: 600,
+        height: 400,
+        maxHeight: 500,
+        maxWidth: 500,
+        content: { url: "/Home/Index2" }
+    };
+    var popupWindow = $popupWindow.kendoWindow(windowConfiguration);
+    popupWindow = popupWindow.data("kendoWindow");
+    $("#popupWindow1_wnd_title").text("New Popup Window1");
     popupWindow.open();
 };
 
@@ -48,51 +207,64 @@ var onClosePopupWindow = function (e) {
     $menuItem.prop("disabled", false);
 };
 
+var selectMenuItem = function (e) {
+    var $item = $(e.item);
+    //var parent = $item.parent("span.k-link").text();
+    //console.log(parent);
+    var menuItemText = $item.children(".k-link").text();
+    console.log("Selected: " + menuItemText);
+
+    var popupWindowConfiguration = getPopupWindowConfiguration(menuItemText);
+    console.log(popupWindowConfiguration);
+};
+
 var onOpen = function(e) {
-    console.log("onOpen");
+    //console.log("onOpen");
 };
 
 var onActivate = function(e) {
-    console.log("onActivate");
+    //console.log("onActivate");
 };
 
 var onRefresh = function(e) {
-    console.log("onRefresh");
+    //console.log("onRefresh");
 };
 
 var onResize = function(e) {
-    console.log("onResize");
+    //console.log("onResize");
 };
 
 var onDragStart = function(e) {
-    console.log("onDragStart");
+    //console.log("onDragStart");
 };
 
 var onDragEnd = function(e) {
-    console.log("onDragEnd");
+    //console.log("onDragEnd");
 };
 
 var onDeactivate = function(e) {
-    console.log("onDeactivate");
+    //console.log("onDeactivate");
 };
 
 var onMinimize = function(e) {
-    console.log("onMinimize");
+    //console.log("onMinimize");
 };
 
 var onMaximize = function(e) {
-    console.log("onMaximize");
-};
-
-var selectMenuItem = function (e) {
-    var menuData = $(e.item).children(".k-link").text();
-    console.log("Selected: " + menuData);
+    //console.log("onMaximize");
 };
 
 //-------------------------------------------------------------
 
+var popupWindowArray = [];
+
 $(document).ready(function () {
+    var numberItems = 6;
+
     attachEventHandlers();
+    popupWindowArray = createPopupWindowArray(numberItems);
+
+    console.log(popupWindowArray);
 });
 
 
